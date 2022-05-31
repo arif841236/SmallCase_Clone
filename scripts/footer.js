f958107be6be850068f94b6b8320c8c63fb24abd
MOHAMMAD ARIF <md970824@gmail.com>
2022-05-31 19:25:43 +0530

    My Small case project final submission

---
+++ scripts/footer.js
@@ -0,0 +1,20 @@
+var form1 = document.getElementById("form01");
+
+form1.addEventListener("submit", submitEmail);
+function submitEmail(event)
+{
+  event.preventDefault();
+
+  $(".alert").removeClass("hide");
+  $(".alert").addClass("show");
+  setTimeout(function () {
+    $(".alert").addClass("hide");
+    $(".alert").removeClass("show");
+  }, 6000);
+  
+};
+
+$(".close-btn").click(function () {
+  $(".alert").addClass("hide");
+  $(".alert").removeClass("show");
+});
